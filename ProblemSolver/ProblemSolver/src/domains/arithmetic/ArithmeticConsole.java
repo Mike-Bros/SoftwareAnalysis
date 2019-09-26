b441d681784bd74641cbbc7a1eebb089196532cb
Mike Bros <mb@rmbros.net>
2019-09-25 22:59:32 -0500

    Framework exercise completed and working properly.

--- ProblemSolver/ProblemSolver/src/domains/arithmetic/ArithmeticConsole.java
+++ ProblemSolver/ProblemSolver/src/domains/arithmetic/ArithmeticConsole.java
@@ -1,14 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package domains.arithmetic;
+
+import framework.ui.ProblemConsole;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
- * @author Mike
+ * @author Mike Bros
  */
-public class ArithmeticConsole {
+public class ArithmeticConsole extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        Scene scene = new Scene(new ProblemConsole(new ArithmeticProblem(), 450, 550));
+        primaryStage.setTitle("Testing Arithmetic Console");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
     
 }
