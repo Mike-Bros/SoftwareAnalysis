67374bd698a98e874c17df352f83e7d4b9274ba4
Mike Bros <mb@rmbros.net>
2019-11-20 23:16:31 -0600

    FarmerIntro implemented needs some tweaking

--- AndroidProjects/ProblemSolver/app/src/main/java/com/example/problemsolver/FarmerIntroActivity.java
+++ AndroidProjects/ProblemSolver/app/src/main/java/com/example/problemsolver/FarmerIntroActivity.java
@@ -2,11 +2,27 @@ package com.example.problemsolver;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import domains.farmer.FarmerProblem;
 
 public class FarmerIntroActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FarmerProblem farmerProblem = new FarmerProblem();
+        String initialState = farmerProblem.getInitialState().toString();
+        String finalState = farmerProblem.getFinalState().toString();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_farmer_intro);
+
+        TextView initState = findViewById(R.id.farmerInitState);
+        initState.setText(initialState);
+        initState.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+
+        TextView finalStateView = findViewById(R.id.farmerFinalState);
+        finalStateView.setText(finalState);
+        finalStateView.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
     }
 }
