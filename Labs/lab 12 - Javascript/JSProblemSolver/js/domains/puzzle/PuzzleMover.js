5bad5d4429e3e3073976bbe972388172641e73ea
Mike Bros <bros21@gmail.com>
2019-12-01 16:16:55 -0600

    Lab 12 renamed from lab 11, lab 12 implemented succefully and submitted

--- Labs/lab 12 - Javascript/JSProblemSolver/js/domains/puzzle/PuzzleMover.js
+++ Labs/lab 12 - Javascript/JSProblemSolver/js/domains/puzzle/PuzzleMover.js
@@ -23,41 +23,65 @@ function PuzzleMover() { }
 PuzzleMover.prototype = puzzleMover;
 
 function slideOne(state) {  // creates a new state with block moved
-    
-    return state;
+    return trySlide(state,1);
 }
 
 function slideTwo(state) {  // creates a new state with block moved
-    
-    return state;
+    return trySlide(state,2);
 }
 
 function slideThree(state) {  // creates a new state with block moved
-    
-    return state;
+    return trySlide(state,3);
 }
 
 function slideFour(state) {  // creates a new state with block moved
-    
-    return state;
+    return trySlide(state,4);
 }
 
 function slideFive(state) {  // creates a new state with block moved
-    
-    return state;
+    return trySlide(state,5);
 }
 
 function slideSix(state) {  // creates a new state with block moved
-    
-    return state;
+    return trySlide(state,6);
 }
 
 function slideSeven(state) {  // creates a new state with block moved
-    
-    return state;
+    return trySlide(state,7);
 }
 
 function slideEight(state) {  // creates a new state with block moved
-    
-    return state;
+    return trySlide(state,8);
+}
+
+function trySlide(state,tileNum){
+    var locTile = state.getLocation(tileNum);
+    var loc0 = state.getLocation(0);
+    if(isAdjecent(locTile,loc0)){
+        var newState = doSwap(locTile,loc0,state);
+        return newState;
+    }
+    return null;
+}
+
+function isAdjecent(locTile,loc0){
+    if(locTile.row === loc0.row){
+        if(locTile.column+1 === loc0.column || locTile.column-1 === loc0.column){
+            return true;
+        }
+    }else if(locTile.column === loc0.column){
+        if(locTile.row+1 === loc0.row || locTile.row-1 === loc0.row){
+            return true;
+        }
+    }else{
+        return false;
+    }
+}
+
+function doSwap(locTile,loc0,state){
+    var tiles = state.tiles;
+    tiles[loc0.row][loc0.column] = tiles[locTile.row][locTile.column];
+    tiles[locTile.row][locTile.column] = 0;
+    var newState = new PuzzleState(tiles);
+    return newState;
 }
